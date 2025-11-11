c4778c8e5f9985cf0fb94274902ed3306daee7fb
Nilesh Domah <ndomah1@gmail.com>
2025-11-11 12:46:44 -0600

    Add files via upload

---
+++ 008. Day 8 - Function Parameters and Caesar Cipher/Functions with Inputs/task.py
@@ -0,0 +1,13 @@
+# No Input(s)
+def greet():
+    print("Hello, ")
+    print("World")
+    print("!")
+
+greet()
+
+# Input(s)
+def greet(name):    # name = parameter
+    print(f"Hey, {name}!")
+
+greet("Angela")     # "Angela" = argumentd
---
+++ 008. Day 8 - Function Parameters and Caesar Cipher/Positional vs Keyword Arguments/task.py
@@ -0,0 +1,15 @@
+# Functions with input
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("Jack Bauer")
+
+
+# Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello, {name}!")
+    print(f"What is it like in {location}?")
+
+greet_with("Jack Bauer", "Nowhere")     # Positional Arguments (default)
+greet_with(location="Nowhere", name="Jack Bauer")     # Keyword Arguments
